a7348bae07d754308062143da0e3a10742ed7186
Daniel McFadyen <shadyshadyshades@hotmail.com>
2024-02-28 12:45:38 +0800

    "Added appbar and image source files"

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
 	import '../app.postcss';
+	import { AppBar } from '@skeletonlabs/skeleton'
 </script>
+
+<AppBar>
+	<a href="/" type="button" class="btn variant-filled">home</a>
+	<a href="/sleepyhead-app" type="button" class="btn variant-filled">sleepyhead app</a>
+	<a href="/about" type="button" class="btn variant-filled">about</a>
+</AppBar>
 
 <slot />
