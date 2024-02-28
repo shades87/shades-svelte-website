927b485d7395e1e3a1b45e83fd446a12d45d73a3
Daniel McFadyen <shadyshadyshades@hotmail.com>
2024-02-28 13:34:30 +0800

    small changes

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,20 @@
+<!--//about--> 
+
+<script>
+    import profile from '$lib/assets/images/grad-pic-circle.png'
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		
+		<img alt="screenshot of app on android phone" src={profile}/>
+
+		<p>Meet Daniel! Daniel (BA, BEd, BComp) is a computer scientist with a passion for AI, mobile application development, and the theory behind computer science. With a keen eye for detail and a knack for problem-solving, Daniel is well-equipped to tackle complex coding challenges and bring innovative ideas to life.
+
+
+            Throughout their university journey, Daniel has gained valuable experience in various programming languages and tools, including Go, Python, Java, Kotlin, and C. Additionally, they have expertise in front-end web development languages, frameworks and markup languages like Javascript, React, HTML and CSS.
+            
+            
+            As a dedicated and collaborative team player, Daniel enjoys working with others to bring projects to fruition. Their strong communication skills and ability to think outside the box make them an asset to any project.</p>
+	</div>
+</div>
---
+++ src/routes/sleepyhead-app/+page.svelte
@@ -0,0 +1,25 @@
+<!--//sleepyhead-app--> 
+
+<script>
+    import screen from '$lib/assets/images/sleepyheadappphone.png'
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		
+		<img alt="screenshot of app on android phone" src={screen}/>
+
+        <h1 class="h1">Use white noise to help you sleep</h1>
+		<p>White noise is a type of sound that contains a blend of frequencies that span the audible range. It is commonly used to help people fall asleep and stay asleep, and there are several benefits of using white noise for this purpose, including:
+
+            Blocks out distracting noises: White noise can help block out distracting noises from the environment, such as traffic or noisy neighbors, which can make it easier for someone to fall asleep and stay asleep.
+            
+            Promotes relaxation: The consistent and soothing sound of white noise can promote relaxation, which can help reduce stress and anxiety and improve the quality of sleep.
+            
+            Creates a familiar environment: White noise can create a familiar environment, similar to the sound of being in the womb, which can be comforting and calming for some people.
+            
+            Reduces sensitivity to other sounds: By masking other sounds, white noise can reduce sensitivity to other sounds that might otherwise wake someone up during the night, such as a dog barking or a door slamming.
+            
+            Improves sleep quality: Studies have shown that using white noise can improve the quality of sleep by reducing the number of times someone wakes up during the night and increasing the amount of time spent in deep sleep. </p>
+	</div>
+</div>
