25deaf172d94c1bbf6f223ff737d67f90477b694
Daniel McFadyen <shadyshadyshades@hotmail.com>
2024-03-01 13:14:45 +0800

    "changes to routing and page file names"

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,8 @@
 <AppBar background="bg-primary-500">
 	<div class="btn-group-horizonta variant-filled-primary">
 		<a href="/" type="button" class="btn variant-ringed">home</a>
-		<a href="/sleepyhead-app" type="button" class="btn variant-ringed">sleepyhead app</a>
-		<a href="/about" type="button" class="btn variant-ringed">about</a>
+		<a href="/sleepyhead-app.html" type="button" class="btn variant-ringed">sleepyhead app</a>
+		<a href="/about.html" type="button" class="btn variant-ringed">about</a>
 	</div>
 </AppBar>
 
