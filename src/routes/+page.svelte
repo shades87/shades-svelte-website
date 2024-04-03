234dc9542ce8027b982f6242ecb43852dbee1ca7
Daniel McFadyen <daniel.m.mcfadyen@gmail.com>
2024-04-03 22:14:21 +0800

    "added star background"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
 	import shades from '$lib/assets/images/shades.png'
+	import star from '$lib/assets/images/star.png'
 </script>
 
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
+<div id="maindiv"  style="background-image: url({star}); ">
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5 justify-center items-center" >
 		<h1 class="h1 justify-center items-center">shadyshadyshades</h1>
@@ -12,12 +12,32 @@
 		<p><code class="code justify-center items-center">shadyshadyshades@hotmail.com</code></p>
 	</div>
 </div>
+</div>
 
 <style>
-	 div {
-    display: flex;
+
+	@keyframes pan {
+		0% {
+			background-position: 0% 0%;
+		}
+
+		100% {
+			background-position: 100% 0%;
+		}
+	}
+
+	#maindiv {
+
+	background-size: 5%;
+	animation: pan 180s linear infinite;
+    
+  }
+	div{
+		display: flex;
     flex-direction: column;
     align-items: center;
-  }
+	}
+
+ 
 
 </style>
