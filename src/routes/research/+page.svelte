04b7ce20f9cad0cd1adc94c35c3a80bb795a3dda
Daniel McFadyen <shadyshadyshades@hotmail.com>
2025-07-03 22:08:12 +1000

    Added research

---
+++ src/routes/research/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+
+
+
+const imgSrc =
+  'https://images.unsplash.com/photo-1463171515643-952cee54d42a?q=80&w=450&h=190&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+
+</script>
+
+<a
+  href="https://dl.acm.org/doi/pdf/10.1145/3701716.3715190"
+  class="mt-5 ml-5 card preset-filled-surface-100-900 border-[1px] border-surface-200-800 card-hover divide-surface-200-800 block max-w-md divide-y overflow-hidden"
+>
+ 
+  <header>
+    <img src={imgSrc} class="aspect-[21/9] w-full grayscale hue-rotate-90" alt="banner" />
+  </header>
+
+  <article class="space-y-4 p-4">
+    <div>
+      <h2 class="h6">Article</h2>
+      <h3 class="h3">Personalised News Summarisation Using Demographic-aware
+BART Model</h3>
+    </div>
+    <p class="opacity-60">
+      This research introduces a novel framework for personalised news summarisation
+that accounts for reader-specific factors such as age, geography, and
+cultural background.
+    </p>
+  </article>
+
+  <footer class="flex items-center justify-between gap-4 p-4">
+    <small class="opacity-60">Presented to ACM WWW Conference Sydney</small>
+    <small class="opacity-60">On 30th April 2025</small>
+  </footer>
+</a>
